6dd144057a797e9448b327b5695aa95d7e4bf8c4
Rosa Ghaedi <153248807+rozaghaedi@users.noreply.github.com>
2025-09-26 15:05:57 +0100

    Update two_sum.py

--- Day01/two_sum.py
+++ Day01/two_sum.py
@@ -1,1 +1,12 @@
+# LeetCode Problem: https://leetcode.com/problems/two-sum/
 
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in seen:
+                return [seen[diff], i]
+            seen[num] = i
