24c079063b9b41d8711a5972de7ebb81844bdd49
Rosa Ghaedi <153248807+rozaghaedi@users.noreply.github.com>
2025-09-26 15:05:37 +0100

    Update palindrome_number.py

--- Day01/palindrome_number.py
+++ Day01/palindrome_number.py
@@ -1,1 +1,9 @@
+# LeetCode Problem: https://leetcode.com/problems/palindrome-number/
 
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # Negative numbers are not palindrome
+        if x < 0:
+            return False
+        # Convert to string and check equality with reverse
+        return str(x) == str(x)[::-1]
