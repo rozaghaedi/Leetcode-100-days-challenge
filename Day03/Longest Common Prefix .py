2ece851d4467c1aa71ff6f1a1e6888933bb385ae
Rosa Ghaedi <153248807+rozaghaedi@users.noreply.github.com>
2025-09-29 13:30:44 +0100

    Update Longest Common Prefix .py

--- Day03/Longest Common Prefix .py
+++ Day03/Longest Common Prefix .py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        for i, ch in enumerate(strs[0]):
+            for s in strs[1:]:
+                if i == len(s) or s[i] != ch:
+                    return strs[0][:i]
+        return strs[0]
