6fee74d2df66f7edbeb13d982676504fb0718610
Rosa Ghaedi <153248807+rozaghaedi@users.noreply.github.com>
2025-10-05 18:44:30 +0100

    Update Valid Parentheses.py

--- Day04/Valid Parentheses.py
+++ Day04/Valid Parentheses.py
@@ -1,1 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        expect={'(':')' , '{':'}' , '[':']'}
+
+        for ch in s:
+            if ch in expect:
+                stack.append(expect[ch])
+            else :
+                if not stack or stack.pop() != ch:
+                    return False
+        return not stack  
+        
 
